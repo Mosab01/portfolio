a142f7a8f4233320ea59b7eb286cd3314406801b
mosab01 <mosabkhraim3@gmail.com>
2023-02-13 00:03:44 +0200

    main

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ function App() {
         <button id="nav-about">Projects</button>
         <button id="nav-projects">Contact</button>
       </div>
-      <div className="main-page">
-        <div className="hello-main">
+        <div className="main-page">
+          <div className="hello-main">
           <h1 id="hello">Hello!</h1>
           <p id="about-sec">I'm Mosab, a full stack developer, who's passionate about creating beautfull and intreactive UI </p>
           <img src={arrow} id="scroll-down"/>
+          </div>
+        </div>
+        <div className="projects">
+          <div className="project-card">
+            <img id="project-img"/>
+            <p id="project-desc">project does this and that</p>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
