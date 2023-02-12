27cdfdca17b407f95f3450cde9eb8de8c5be3f0f
mosab01 <mosabkhraim3@gmail.com>
2023-02-13 00:03:30 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
+import arrow from "./arrow.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="nav-bar">
-        <p>mosab khraim</p>
-        <p id="github">github</p>
-        <p>linkdin</p>
+        <button id="nav-about">Projects</button>
+        <button id="nav-projects">Contact</button>
+      </div>
+      <div className="main-page">
+        <div className="hello-main">
+          <h1 id="hello">Hello!</h1>
+          <p id="about-sec">I'm Mosab, a full stack developer, who's passionate about creating beautfull and intreactive UI </p>
+          <img src={arrow} id="scroll-down"/>
+        </div>
       </div>
     </div>
   );
 }
+
 export default App;
