892006bb4646c082e71658df3689616f74e40ad9
mosa01 <mosabkhraim3@gmail.com>
2023-02-14 22:17:30 +0200

    main

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ export default function App() {
       <NavBar/>
       <Main_page/>
       <Projects/>
+
+      <div className="contact">
+        <h1>Say Hello</h1>
+        <div className="contact-forum">
+          <input id="full-name" placeholder="Full Name"/>
+          <input id="email-adress" placeholder="Email Adress"/>
+          <input id="messege-subject" placeholder="Messege Subject"/>
+          <input id="messege" placeholder="Messge"/>
+
+          <button id="send-btn">SEND</button>
+        </div>
+      </div>
     </div>
   );
 }
