68ac251369b2ac31f84244d15ba3eef196823166
mosa01 <mosabkhraim3@gmail.com>
2023-02-14 10:49:31 +0200

    main

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,24 @@
 import React from "react";
 import img1 from "../images/card.jpg";
-import  { useState } from 'react';
 
 export default function Projects() {
-
-    const [isShown, setIsShown] = useState(false);
-
-
+    
 return(
 <div className="projects">
-    <div className="project-card" id="card-1"         onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}>
-        <img src={img1} id="card-img"/>
-        {isShown && (
-        <p id="card-desc">this is a descrption</p>
-      )}
-
-
+    <div className="project-card" id="card-1">
+        {/* <img src={img1} id="card-img" alt="project_img"/> */}
     </div>
     
     <div className="project-card" id="card-2">
-        <img src={img1} id="card-img"/>
-        <p id="card-desc">this is a descrption</p>
+        {/* <img src={img1} id="card-img" alt="project_img"/> */}
     </div>
 
     <div className="project-card" id="card-3">
-        <img src={img1} id="card-img"/>
-        <p id="card-desc">this is a descrption</p>
+        {/* <img src={img1} id="card-img" alt="project_img"/> */}
     </div>
 
     <div className="project-card" id="card-4">
-        <img src={img1} id="card-img"/>
-        <p id="card-desc">this is a descrption</p>
+        {/* <img src={img1} id="card-img" alt="project_img"/> */}
     </div>
 </div>
 )}
