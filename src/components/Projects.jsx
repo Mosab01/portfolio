65d842a132148fd65ef45acc3091a12202aff0b4
mosab01 <mosabkhraim3@gmail.com>
2023-02-20 00:31:26 +0200

    main

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import img1 from "../images/card-1.jpg";
-import img2 from "../images/card-2.jpg";
+import Qr_generator from "../images/Qr_generator.png";
 import img3 from "../images/card-3.jpg";
 import img4 from "../images/card-4.jpg";
+import weather from "../images/Weather_site.png";
 import "./Projects.css";
 
 export default function Projects() {
@@ -10,7 +10,7 @@ export default function Projects() {
 return(
 <div className="projects">
     <div className="project-card" id="card-1">
-        <img src={img1} id="card-img" alt="project_img"/>
+        <img src={weather} id="card-img" alt="project_img"/>
         <div id="bg-desc"/>
         <p id="project-desc">
                 Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
@@ -19,7 +19,7 @@ return(
     </div>
     
     <div className="project-card" id="card-2">
-        <img src={img2} id="card-img" alt="project_img"/>
+        <img src={Qr_generator} id="card-img" alt="project_img"/>
         <div id="bg-desc"/>
         <p id="project-desc">
                 Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
